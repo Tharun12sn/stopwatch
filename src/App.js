d81ae01a810a08b8570d95b515bfed3b0ce93371
Gurram Tharun <gurramtharun649@hmail.com>
2025-05-20 19:00:21 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
+import {Component} from "react";
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends Component{
+  state={count:0}
+  onperfect=()=>{
+  this.setState((prev)=>({count:prev.count+1}));
+  }
+  nonperfect=()=>{
+    // const {count}=this.state
+    //  if(count>0){
+    //  this.setState((prev)=>({count:prev.count-1}));
+    //  }
+    //  else{
+    //   this.setState((prev)=>({count:prev.count}));
+    //  }
+    this.setState((prev)=>({count:prev.count-1}));
+  }
+  render(){
+    const {count}=this.state
+  return(
+    <div className="container">
+      <h1>The Button has been clicked  <span> {count}</span> times</h1>
+      <p>bcdhvbfhvbfvbfbvf</p>
+      <button type="button" onClick={this.onperfect}>Increase</button>
+      <button type="button" onClick={this.nonperfect}>Decrease</button>
+     
+        {count>0 || count<0 ? (<button type="button">ReStart</button>):null }
+      
+      
     </div>
-  );
+
+  )
 }
+}
+
 
 export default App;
